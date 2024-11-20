f0618d9c91246b4420171dea4f9078d95b7d114e
hoangglam94 <82983550+hoangglam94@users.noreply.github.com>
2024-11-20 14:51:36 -0600

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -30,27 +30,76 @@ app.use(cors(
 
 ))
 
+/* Login heard */
 app.post('/login', (req, res) => {
 const  { email, password } = req.body;
 console.log({ email,  password });
 const sql = "SELECT * from Employees Where eEmail = ? and PasswordHash = ?"
+const sql2 = "SELECT * from Employees where eemail = ? and passwordhash = ? and manager=TRUE;"
 db.query(sql, [email, password], (err, result) => {
-    if (err){
-        res.json({loginStatus: false, Error: "Query Error"  })
+    if (err) {
+        return res.json({ loginStatus: false, Error: "Query Error" });
     }
-    if (result.length > 0){
- 
 
-        return res.json({loginStatus: true  })
-    } else{
-        return res.json({loginStatus: false, Error: "Wrong emails/passwords"    })
+    if (result.length > 0) {
+        // User exists, now check if they are a manager
+        db.query(sql2, [email, password], (err, managerResult) => {
+            if (err) {
+                return res.json({ loginStatus: false, Error: "Query Error" });
+            }
+
+            if (managerResult.length > 0) {
+                return res.json({ loginStatus: true, Admin: true });
+            } else {
+                return res.json({ loginStatus: true, Admin: false });
+            }
+        });
+    } else {
+        // User does not exist
+        return res.json({ loginStatus: false, Error: "Wrong email/password" });
     }
+});
 })
+
+/* Signup heard */
+
+app.post('/signup' , (req,res) =>{
+    const  { name, email, password } = req.body;
+    console.log({ name, email, password })
+    const sql3 = "INSERT INTO Employees(eName,eEmail,PasswordHash) VALUES(?,?,?);";
+    db.query(sql3, [name,email,password] , (err, result) =>{
+        if (err) {
+            return res.json({ signUpStatus: false, Error: "Error. Try another email input" });
+        }
+
+        if(result){
+            console.log("Created new account as:")
+            console.log({ name, email, password })
+            return res.json({ signUpStatus: true})
+        }else{
+            return res.json({ signUpStatus: false, Error: "Unable to create accout. Please try another." });
+        }
+    })
+
+        
+})
+
+
+/* Signup heard */
+app.post('/dashboard/assignskill', (req,res) =>{
+    const result = req.body;
+    console.log(req.body)
+    const skillstring = result.skills.join(', ');
+    console.log(skillstring);
+
     
+
+
 })
+
+
+
 
 app.listen(3000, () => {
     console.log("Server is running")
 })
-
-
