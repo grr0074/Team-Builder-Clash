346e090b6a1e7ec2ca6e10c1e8e1b930b449e3f7
hoangglam94 <82983550+hoangglam94@users.noreply.github.com>
2024-11-30 13:34:21 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -49,8 +49,78 @@ db.query(sql, [email, password], (err, result) => {
     
 })
 
-app.listen(3000, () => {
-    console.log("Server is running")
+/* Signup heard */
+
+app.post('/signup' , (req,res) =>{
+    const  { name, email, password } = req.body;
+    console.log({ name, email, password })
+    const sql3 = "INSERT INTO Employees(eName,eEmail,PasswordHash) VALUES(?,?,?);";
+    db.query(sql3, [name,email,password] , (err, result) =>{
+        if (err) {
+            return res.json({ signUpStatus: false, Error: "Error. Try another email input" });
+        }
+
+        if(result){
+            console.log("Created new account as:")
+            console.log({ name, email, password })
+            return res.json({ signUpStatus: true})
+        }else{
+            return res.json({ signUpStatus: false, Error: "Unable to create accout. Please try another." });
+        }
+    })
+
+        
 })
 
 
+/* Assignskill heard */
+app.post('/dashboard/assignskill', (req,res) =>{
+    const result = req.body;
+    console.log(req.body)
+    const skillstring = result.skills.join(', ');
+    console.log(skillstring);
+
+    
+
+
+})
+
+/* Get Projects heard */
+
+app.get('/api/projects', (req, res) => {
+    const sql4 = "SELECT PJID, pName from Projects;";
+    db.query(sql4, (err, results) => {
+        if (err) {
+            console.error('Error executing query: ', err);
+            return res.status(500).json({ error: 'Database query failed' });
+        }
+        const projects = results.map(row => ({
+            id: row.PJID,
+            projectName: row.pName
+        }));        
+        console.log(projects)
+
+        res.json(projects);
+
+    });
+});
+
+app.get('/api/get-email', (req, res) => {
+     // Log the cookies to see what is received
+//    console.log("Cookies:", req.cookies);
+
+    const token = req.cookies.token; // Access the cookie
+    if (!token) {
+        return res.status(401).json({ Error: "Unauthorized" });
+    }
+    try {
+        const decoded = jwt.verify(token, "jwt_secret_key");
+        res.json({ email: decoded.email });
+    } catch (error) {
+        return res.status(401).json({ Error: "Invalid token" });
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Server is running")
+})
