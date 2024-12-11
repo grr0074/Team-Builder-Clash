e1cc99bf916ac3e87643cc1bdcf033984606af5e
grr0074 <130199505+grr0074@users.noreply.github.com>
2024-12-10 20:32:19 -0600

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1 className="welcome-message">Welcome to Team Builder Clash!</h1>
+    </div>
+  );
+};
+
+export default Home;
